ec139ac7f2f0fc83e65368e1d318b4c6a056a1a1
Paul <pwatteau@etu.u-bordeaux.fr>
2022-12-08 15:17:03 +0100

    correction de bouf

--- cadoizzob/text.py
+++ cadoizzob/text.py
@@ -323,6 +323,6 @@ MK8DXTotalMap = { "mks" : "Mario Kart Stadium" , 'wp' :"Water Park" , 'ssc' : "S
                 "bnym" : "MKT New York" , "bmc3" : "SNES Mario Circuit 3" , "bkd" : "N64 Kalimari Desert" , "bwp" : "DS Flipper Waluigi", #troisième coupe
                 "bss" : "MKT Sydney", "bsl" : "GBA Snow Land" , "bmg" : "WII Mushroom Gorge" , "bshs" :"Sky High Sundae", #4ème coupe
                 "bll" : "London Loop", "bbl" : "Boo Lake" ,"bap" : "Alpine Pass", "bmt" : "Maple Treeway" ,  # 5ème coupe
-                "bbb" : "Berlin Byways", "bpg" : "Peach Gardens", "bmm" : "Merry Mountain", "bbr7" : "Rainbow Road" #6eme coupe 
+                "bbb" : "Berlin Byways", "bpg" : "Peach Gardens", "bmm" : "Merry Mountain", "bbr7" : "Rainbow Road", #6eme coupe 
                 'week' :"Map of the Week" 
 }
